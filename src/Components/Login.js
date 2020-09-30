3aba2f7ef41ca75039a81c6193a99a96a07f5114
roman9891 <roman9891@gmail.com>
2020-09-30 10:45:04 -0400

    create works

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -17,11 +17,52 @@ class Login extends Component {
 
     submitHandler = (e) => {
         e.preventDefault()
-        if (this.getUser()) {this.props.appHandler(this.getUser())}    
+        // if (e.target.login) {
+        //     console.log('login')
+        // }
+        // if (e.target.create) {
+        //     console.log('create')
+        // }
+        // console.log(e.target.value)
+        // if (this.getUser()) {this.props.appHandler(this.getUser())}    
+    }
+
+    clickHandler = (e) => {
+        // console.log(e.target.value)
+        if (e.target.name === 'login') {
+            this.getUser()
+        }
+        if (e.target.name === 'create') {
+            this.postUser()
+        }
     }
 
     getUser = () => {
-        return {username: this.state.username, progress: 10}
+        fetch(`http://localhost:3000/api/v1/users`, {
+            method: `GET`,
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+            },
+            body: JSON.stringify({user: {...this.state}})
+        })
+        .then(r => r.json())
+        .then(user => this.props.appHandler(user))
+        // return {username: this.state.username, progress: 10}
+    }
+
+    postUser = () => {
+        fetch(`http://localhost:3000/api/v1/users`, {
+            method: `POST`,
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+            },
+            body: JSON.stringify({user: {...this.state, progress: 1}})
+        })
+        .then(r => r.json())
+        .then(user => this.props.appHandler(user))
+        // return {username: this.state.username, progress: 10}
     }
 
     render() {
@@ -33,8 +74,10 @@ class Login extends Component {
                     <label>Password</label>
                     <input type='password' name='password' onChange={this.changeHandler} value={this.state.password}></input>
                     <br/>
-                    <button type='submit'>Login</button>
-                    <button type='submit'>Create Account</button>
+                    <button onClick={this.clickHandler} name='login'>Login</button>
+                    <button onClick={this.clickHandler} name='create'>Create Account</button>
+                    {/* <input type='submit' name='login' value='Login'/> */}
+                    {/* <input type='submit' name='create' value='Create'/> */}
                 </form>
             </div>
         );
